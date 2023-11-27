de0a91c781e3dba0c7dbfd7c02d33940bc01aa0f
Padilla <a_franciscojavier.padilla.lopez@iespablopicasso.es>
2023-11-27 08:40:12 +0100

    Añado los primeros ficheros

---
+++ src/programa1.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+public class programa1 {
+
+	public static void main(String[] args) {
+		Random random = new Random();
+		
+		int i = random.nextInt(1, 10);
+		System.out.println(i);
+
+	}
+
+}
